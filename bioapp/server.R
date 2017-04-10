011b868b818f2576130576ab7d161ed1a3ee4430
mggrami <grabski.mic@gmail.com>
2017-04-10 12:52:50 +0200

    bioapp

--- bioapp/server.R
+++ bioapp/server.R
@@ -1,8 +1,14 @@
 library(shiny)
 odczyt<- read.csv("data/1_VS_2_16_USUNIETE_ODCZYTY_R.csv", header= TRUE, sep="\t")
+odczyt2<- read.csv("data/1_VS_3_47_USUNIETE_ODCZYTY_R.csv", header = TRUE, sep="\t")
 
 shinyServer(function(input, output){
   
-  output$dane <- renderTable(odczyt)
-  
+  dataInput<- reactive({
+    switch (input$data,
+      "PNP" = odczyt,
+      "RNR" = odczyt2)
+  })
+  output$dane <-renderTable(
+    {head(dataInput(), n=10)})
 })
--- bioapp/ui.R
+++ bioapp/ui.R
@@ -3,7 +3,17 @@ library(shiny)
 shinyUI(fluidPage(
   titlePanel("Dane RNAseq"),
   
-   mainPanel(
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("data",
+                 label = "Wybierz zmutowany szczep E.coli:",
+                 choices = c("PNP", "RNR"),
+                 selected="PNP"
+    )
+  ),
+  mainPanel(
+    h2("Dane"),
     tableOutput("dane")
+  )
   )
 ))
