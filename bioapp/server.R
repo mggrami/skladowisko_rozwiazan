e94b9dd09a992dd72c786605db85a5fbece1344b
mggrami <grabski.mic@gmail.com>
2017-04-14 14:07:21 +0200

    bioapp

--- bioapp/server.R
+++ bioapp/server.R
@@ -17,7 +17,18 @@ shinyServer(function(input, output){
   output$dane <-renderTable(
     {head(dataInput(), n=input$obs)})
   
+  wyboryInput<- reactive({
+    switch(input$wybory,
+           "Transcription Start"= dataInput()$Transcription.Start,	"Translation Start"= dataInput()$Translation.Start,	"Translation Stop"= dataInput()$Translation.Stop,	"Transcription Stop"= dataInput()$Transcription.Stop,	"Strand"= dataInput()$Strand,	"Name"= dataInput()$Name,	"Synonym"= dataInput()$Synonym,	"Product"= dataInput()$Product,	"Raw Counts 1"= dataInput()$Raw.Counts.1,	"Normalized Counts 1"= dataInput()$Normalized.Counts.1,	"RPKM 1"= dataInput()$RPKM.1,	"Expression 1"= dataInput()$Expression.1,	"Raw Counts 2"= dataInput()$Raw.Counts.2,	"Normalized Counts 2"= dataInput()$Normalized.Counts.2,	"RPKM 2"= dataInput()$RPKM.2,	"Expression 2"= dataInput()$Expression.2,	"Raw Counts 3"= dataInput()$Raw.Counts.3,	"Normalized Counts 3"= dataInput()$Normalized.Counts.3,	"RPKM 3"= dataInput()$RPKM.3,	"Expression 3"= dataInput()$Expression.3,	"Raw Counts 4"= dataInput()$Raw.Counts.4,	"Normalized Counts 4"= dataInput()$Normalized.Counts.4,	"RPKM 4"= dataInput()$RPKM.4,	"Expression 4"= dataInput()$Expression.4,	"pValue 1 vs 2"= dataInput()$pValue.1.vs.2,	"qValue 1 vs 2"= dataInput()$qValue.1.vs.2,	"pValue 1 vs 3"= dataInput()$pValue.1.vs.3,	"qValue 1 vs 3"= dataInput()$qValue.1.vs.3,	"pValue 1 vs 4"= dataInput()$pValue.1.vs.4,	"qValue 1 vs 4"= dataInput()$qValue.1.vs.4,	"pValue 2 vs 3"= dataInput()$pValue.2.vs.3,	"qValue 2 vs 3"= dataInput()$qValue.2.vs.3,	"pValue 2 vs 4"= dataInput()$pValue.2.vs.4,	"qValue 2 vs 4"= dataInput()$qValue.2.vs.4,	"pValue 3 vs 4"= dataInput()$pValue.3.vs.4,	"qValue 3 vs 4"= dataInput()$qValue.3.vs.4,	"fold.change"= dataInput()$fold.change,	"log2.fold.change"= dataInput()$log2.fold.change)
+  })
+  
+  wybory2Input<- reactive({
+    switch(input$wybory2,
+           "Transcription Start"= dataInput()$Transcription.Start,	"Translation Start"= dataInput()$Translation.Start,	"Translation Stop"= dataInput()$Translation.Stop,	"Transcription Stop"= dataInput()$Transcription.Stop,	"Strand"= dataInput()$Strand,	"Name"= dataInput()$Name,	"Synonym"= dataInput()$Synonym,	"Product"= dataInput()$Product,	"Raw Counts 1"= dataInput()$Raw.Counts.1,	"Normalized Counts 1"= dataInput()$Normalized.Counts.1,	"RPKM 1"= dataInput()$RPKM.1,	"Expression 1"= dataInput()$Expression.1,	"Raw Counts 2"= dataInput()$Raw.Counts.2,	"Normalized Counts 2"= dataInput()$Normalized.Counts.2,	"RPKM 2"= dataInput()$RPKM.2,	"Expression 2"= dataInput()$Expression.2,	"Raw Counts 3"= dataInput()$Raw.Counts.3,	"Normalized Counts 3"= dataInput()$Normalized.Counts.3,	"RPKM 3"= dataInput()$RPKM.3,	"Expression 3"= dataInput()$Expression.3,	"Raw Counts 4"= dataInput()$Raw.Counts.4,	"Normalized Counts 4"= dataInput()$Normalized.Counts.4,	"RPKM 4"= dataInput()$RPKM.4,	"Expression 4"= dataInput()$Expression.4,	"pValue 1 vs 2"= dataInput()$pValue.1.vs.2,	"qValue 1 vs 2"= dataInput()$qValue.1.vs.2,	"pValue 1 vs 3"= dataInput()$pValue.1.vs.3,	"qValue 1 vs 3"= dataInput()$qValue.1.vs.3,	"pValue 1 vs 4"= dataInput()$pValue.1.vs.4,	"qValue 1 vs 4"= dataInput()$qValue.1.vs.4,	"pValue 2 vs 3"= dataInput()$pValue.2.vs.3,	"qValue 2 vs 3"= dataInput()$qValue.2.vs.3,	"pValue 2 vs 4"= dataInput()$pValue.2.vs.4,	"qValue 2 vs 4"= dataInput()$qValue.2.vs.4,	"pValue 3 vs 4"= dataInput()$pValue.3.vs.4,	"qValue 3 vs 4"= dataInput()$qValue.3.vs.4,	"fold.change"= dataInput()$fold.change,	"log2.fold.change"= dataInput()$log2.fold.change)
+  })
+  
   output$plot1 <- renderPlot({
-    ggplot(dataInput(), aes(x=Name, y=log2.fold.change))+ geom_point()
+    ggplot(dataInput(), aes(x=wyboryInput(), y=wybory2Input()))+ geom_point()+
+      theme(axis.text.x = element_text(angle = 90, hjust = 1))
   })
 })
