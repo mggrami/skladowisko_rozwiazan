f6526d70b5fe65e03af2dc7d27026d4853d08b79
mggrami <grabski.mic@gmail.com>
2017-05-04 17:17:42 +0200

    Aplikacja_02

---
+++ Aplikacja_02/ui.R
@@ -0,0 +1,22 @@
+shinyUI(fluidPage(
+  titlePanel("Zaawansowane Shiny"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("data",
+                  label = "Wybierz zmutowany szczep E.coli:",
+                  choices = c("PNP", "RNR", "RNB"),
+                  selected="PNP")
+    ),
+  mainPanel(
+    h2("Dane"),
+    
+    dataTableOutput("dane2"),
+    
+    plotlyOutput("wizual"),
+    
+    dataTableOutput("izolacja")
+  )
+)
+))
+
